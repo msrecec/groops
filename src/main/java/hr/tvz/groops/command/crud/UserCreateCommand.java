5c00d2d48a6f0c6d2281823634740366cbc0499b
msrecec <msrecec@3plus.hr>
2023-07-11 04:02:51 +0200

    qf

--- src/main/java/hr/tvz/groops/command/crud/UserCreateCommand.java
+++ src/main/java/hr/tvz/groops/command/crud/UserCreateCommand.java
@@ -12,6 +12,9 @@ import java.sql.Date;
 @ToString
 public class UserCreateCommand extends UserCommand {
 
+    public UserCreateCommand() {
+    }
+
     public UserCreateCommand(String username, String email, String firstName, String lastName, Date dateOfBirth, String description, String password) {
         super(username, email, firstName, lastName, dateOfBirth, description);
         this.password = password;
