d03ffa895cde3a859226f794359971c046855d34
msrecec <msrecec@3plus.hr>
2023-08-28 02:17:50 +0200

    qf

--- src/main/java/hr/tvz/groops/controller/AuthenticationController.java
+++ src/main/java/hr/tvz/groops/controller/AuthenticationController.java
@@ -3,6 +3,8 @@ package hr.tvz.groops.controller;
 import hr.tvz.groops.command.crud.LoginCommand;
 import hr.tvz.groops.dto.response.LoginDto;
 import hr.tvz.groops.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +13,7 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/authentication")
 public class AuthenticationController extends ControllerBase {
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
     private final UserService userService;
 
     public AuthenticationController(UserService userService) {
@@ -21,6 +24,11 @@ public class AuthenticationController extends ControllerBase {
     LoginDto login(@RequestBody @Valid LoginCommand command, HttpServletResponse response) {
         LoginDto loginDto = userService.login(command.getUsername(), command.getPassword(), response);
         return LoginDto.builder().exp(loginDto.getExp()).build();
+    }
+
+    @GetMapping("/nop")
+    void nop() {
+        logger.debug("Running nop...");
     }
 
     @DeleteMapping("/logout")
