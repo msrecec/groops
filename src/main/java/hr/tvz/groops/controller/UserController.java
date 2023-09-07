f2ccc2d3cd2a4a228d1956414b519360698ec2ab
msrecec <msrecec@3plus.hr>
2023-09-07 18:33:37 +0200

    qf

--- src/main/java/hr/tvz/groops/controller/UserController.java
+++ src/main/java/hr/tvz/groops/controller/UserController.java
@@ -16,7 +16,6 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.validation.*;
 import java.io.IOException;
 import java.util.List;
-import java.util.Set;
 
 @RestController
 @RequestMapping("/users")
@@ -55,14 +54,10 @@ public class UserController extends ControllerBase {
     }
 
     @PostMapping("/current/upload-profile")
-    UserDto uploadProfilePicture(@RequestParam("file1") MultipartFile file1, @RequestParam("file2") MultipartFile file2) throws IOException {
-        UserUpdateCommand userUpdateCommand = objectMapper.readValue(file2.getInputStream(), UserUpdateCommand.class);
+    UserDto uploadProfilePicture(@RequestParam("file") MultipartFile file, @RequestParam("command") MultipartFile command) throws IOException {
+        UserUpdateCommand userUpdateCommand = objectMapper.readValue(command.getInputStream(), UserUpdateCommand.class);
         validationService.validate(userUpdateCommand);
-        return userService.update(userUpdateCommand, file1);
-    }
-
-    private UserCommand isValid(@Valid UserCommand command) {
-        return command;
+        return userService.update(userUpdateCommand, file);
     }
 
     @PutMapping("/current")
